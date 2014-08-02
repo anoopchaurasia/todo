c5439a5b2b5c0a263aabf5abc278c34f9084d40d
anoopchaurasia <anoop26667@gmail.com>
2014-08-02 15:22:20 +0530

    Added inline editing

--- web/js/src/classes/common/InlineEditor.js
+++ web/js/src/classes/common/InlineEditor.js
@@ -1,48 +1,69 @@
 fm.Package("common");
 fm.Class("InlineEditor");
 common.InlineEditor = function () {
-	
+
 	function createTextArea(  div ) {
 		var text = $.trim(div.html()).replace(	/\s+/g, " ").replace(/<br\s*\/?>/mgi,"\n");
 		$(this).hide();
+        div.css("position", "relative");
 		var pos = $( div ).position();
 		var height = 0;
 		var html = "<textarea>" + text + "</textarea>";
-		var textarea = $( html ).insertBefore( "body" );
+		var textarea = $( html ).appendTo(div.parent());
+        var divPaddingHeight = parseInt(div.css("padding-top")) + parseInt(div.css("padding-bottom"));
+        var divPaddingWidth = parseInt(div.css("padding-left")) + parseInt(div.css("padding-right"));
 		textarea.css({
 			left:pos.left + parseInt( div.css("margin-left")) -1,
-			top:pos.top + parseInt( div.css("margin-top")) -1 ,
+			top:pos.top + parseInt( div.css("margin-top")) -1,
 			position:"absolute",
-			width:div.width() ,
-			height:div.height(),
-			"max-height": div.height(),
+			width:div.width() + divPaddingWidth,
+			height:div.height() + divPaddingHeight,
+            "padding-top": div.css("padding-top"),
+            "padding-left": div.css("padding-left"),
+            "padding-bottom": div.css("padding-bottom"),
+            "padding-right": div.css("padding-right"),
+			"max-height": div.height() + divPaddingHeight,
+            "word-break": "break-word",
 			"font-size": div.css("font-size"),
 			"font-weight":div.css("font-weight"),
 			display:"block",
 			border:"1px solid #ccc",
 			"font-family":div.css("font-family"),
 			"overflow-x": "auto",
-			"overflow-y": "hidden"
+			"overflow-y": "hidden",
+            resize: "none"
 		}).blur( function() {
 			div.html( this.value.replace(/\n/g, "<br>") );
 			div.css("visibility","visible");
 			$( this).remove();
 		}).keyup(function(e) {
-			
+            div.html( this.value.replace(/\n/g, "<br>") );
 			if( height !=  this.scrollHeight ){
 				$(this).height( this.scrollHeight ).css( "max-height", this.scrollHeight);
-			}		
+			}
 		});
 		textarea.focus();
+        moveCaretToEnd(textarea[0])
 		div.css("visibility","hidden");
-		 
+
 	}
-	
+
+    function moveCaretToEnd(el) {
+        if (typeof el.selectionStart == "number") {
+            el.selectionStart = el.selectionEnd = el.value.length;
+        } else if (typeof el.createTextRange != "undefined") {
+            el.focus();
+            var range = el.createTextRange();
+            range.collapse(false);
+            range.select();
+        }
+    }
+
 	function makeEditable() {
 		createTextArea( $( this ) );
 	}
 	this.InlineEditor = function(element) {
-		
+
 		element.click(makeEditable);
 	};
 };
--- web/js/src/classes/todo/list/ListManager.js
+++ web/js/src/classes/todo/list/ListManager.js
@@ -2,13 +2,14 @@ fm.Package("todo.list");
 fm.Import("todo.list.List");
 fm.Class("ListManager", "abstract.ItemList");
 todo.list.ListManager = function (base, me, List) {
-	
+
 	this.ListManager = function (lists) {
 		this.base(List, lists || []);
-		this.add(new List());
-		this.add(new List());
-		this.add(new List());
-		this.add(new List());
-		this.add(new List());
+        this.addList();
 	};
+
+    this.addList = function (){
+        this.add(new List());
+    };
+
 };
